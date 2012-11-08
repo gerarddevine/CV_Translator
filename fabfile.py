cc1c5291a3bda48e292a1eb2ccbc5d618c8c08d3
Gerard Devine <g.m.devine@reading.ac.uk>
2012-11-08 12:51:24 +0000

    updating lxml version

--- fabfile.py
+++ fabfile.py
@@ -14,17 +14,18 @@ def gitupdate():
 
 
 def deploy():
-    code_dir = '/home/gdevine/web/prod/cv_translator'
+    proj_code_dir = '/home/gdevine/web/prod/cv_translator'
+    app_code_dir = '/home/gdevine/web/prod/cv_translator/cv_translator'
     with settings(warn_only=True):
-        if run("test -d %s" % code_dir).failed:
-            run("git clone git@github.com:gerarddevine/CV_Translator.git %s" % code_dir)
-    with cd(code_dir):
+        if run("test -d %s" % proj_code_dir).failed:
+            run("git clone git@github.com:gerarddevine/CV_Translator.git %s" % proj_code_dir)
+    with cd(proj_code_dir):
         run("pwd")
         run("ls -la")
         run("git pull")
         run("virtualenv venv --no-site-packages")
-        run(env.activate + "&& pip install -r requirements.txt")
-        run(env.activate + "cd cv_translator")
-        run(env.activate + "python manage.py syncdb --noinput")
-        run(env.activate + "chmod 777 cv_translator.sqlite")
+        run(env.activate + " && pip install -r requirements.txt")
+    with cd(app_code_dir):
+        run(env.activate + " && python manage.py syncdb --noinput")
+        run(env.activate + " && chmod 777 cv_translator.sqlite")
 
