fd7c48ac7d7ca8c0c2b423a539cfd71c72ebd6f6
Gerard Devine <g.m.devine@reading.ac.uk>
2012-11-27 11:18:06 +0000

    Some minor code tidy-up

--- cv_translator/apps/translator/mindmap.py
+++ cv_translator/apps/translator/mindmap.py
@@ -2,8 +2,6 @@ import re
 import os
 from lxml import etree
 
-import libxml2
-import libxslt
 from subprocess import *
 import datetime
 
@@ -73,78 +71,32 @@ def translateMM(mmfile):
     ''' translate mindmap to qn xml
     
     '''
-
+  
     foutname = os.path.join(thisDir, 'xslt', str(mmfile).replace(".mm",".xml"))
     #pull in the xsl transformation file
     XSLFileName = os.path.join(thisDir, 'xslt', 'mm2q_bdl.xsl')
 
     #pull in the mindmap file to be translated
-    #fin = open(os.path.join(thisDir, 'xslt', str(mmfile)), 'r')
     fin = mmfile
-    
-    #determine the svn info of the mindmap file
-    mmurl=""
-    revision=""
-    mmlcrevision=""
-    svninforaw = Popen(["svn", "info" , str(mmfile)], stdout=PIPE).communicate()[0]
-    svninfo = svninforaw.split("\n")
-    for line in svninfo:
-      if re.match("^Revision:",line):
-        print line
-        revsplit=line.split(": ")
-        revision=revsplit[1]
-      if re.match("^Last Changed Rev:",line):
-        print line
-        lcrevsplit=line.split(": ")
-        mmlcrevision=lcrevsplit[1]
-      if re.match("^URL:",line):
-        print line
-        urlsplit=line.split(": ")
-        mmurl=urlsplit[1]
-    
-    transrevision=""
-    transurl=""
-    svninforaw = Popen(["svn", "info" , str(mmfile)], stdout=PIPE).communicate()[0]
-    svninfo=svninforaw.split("\n")
-    for line in svninfo:
-      if re.match("^Revision:",line):
-        print line
-        revsplit=line.split(": ")
-        transrevision=revsplit[1]
-      if re.match("^URL:",line):
-        print line
-        urlsplit=line.split(": ")
-        transurl=urlsplit[1]
-    
+
     fpre = open(os.path.join(thisDir,'xslt', str(mmfile)+'.pre'), 'w')
-                
+
     for line in fin:
       if re.match("^<text>",line) or re.match("^<richcontent TYPE=",line):
         line=line.replace('[','<')
         line=line.replace(']','>')
       fpre.write(line)
     fpre.close()
-    
-    couple="'no'"
-    
-    styledoc = libxml2.parseFile(XSLFileName)
-    style = libxslt.parseStylesheetDoc(styledoc)
-    doc = libxml2.parseFile(fpre.name)
-    #result = style.applyStylesheet(doc,{"Couple" : couple, "Revision" : revision, "URL" : "'"+mmurl+"'", "LCRevision" : mmlcrevision, "TranslatorRevision" : transrevision, "TranslatorURL" : "'"+transurl+"'", "Date" : "'"+str(datetime.datetime.now())+"'"})
-    result = style.applyStylesheet(doc, None)
-    
-    # To save to a file, uncomment and edit the following line
-    #style.saveResultToFilename(foutname, result, 0)
-    strResult = style.saveResultToString(result)
-    
-    style.freeStylesheet()
-    doc.freeDoc()
-    result.freeDoc()
-    
+
+    xslt_root = etree.parse(XSLFileName)
+    xml_input = etree.parse(fpre.name)
+    transform = etree.XSLT(xslt_root)
+    strResult = transform(xml_input)
+
     os.remove(os.path.join(thisDir,'xslt', str(mmfile)+'.pre'))
-    
+
     return strResult
-
-    
-
-    
+  
+  
+  
+  
